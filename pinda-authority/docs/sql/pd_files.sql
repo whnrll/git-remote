04da39673497fe4988b3517574df00e440e77645
xutao <xutao001225@163.com>
2023-09-14 22:11:27 +0800

    一些代码

---
+++ pinda-authority/docs/sql/pd_files.sql
@@ -0,0 +1,79 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : ����MySQL���ݿ�
+Source Server Version : 50728
+Source Host           : localhost:3306
+Source Database       : pd_files
+
+Target Server Type    : MYSQL
+Target Server Version : 50728
+File Encoding         : 65001
+
+Date: 2020-04-29 16:42:02
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `pd_attachment`
+-- ----------------------------
+DROP TABLE IF EXISTS `pd_attachment`;
+CREATE TABLE `pd_attachment` (
+  `id` bigint(20) NOT NULL COMMENT 'ID',
+  `biz_id` varchar(64) DEFAULT NULL COMMENT '业务ID',
+  `biz_type` varchar(255) DEFAULT NULL COMMENT '业务类型\n#AttachmentType',
+  `data_type` varchar(255) DEFAULT 'IMAGE' COMMENT '数据类型\n#DataType{DIR:目录;IMAGE:图片;VIDEO:视频;AUDIO:音频;DOC:文档;OTHER:其他}',
+  `submitted_file_name` varchar(255) DEFAULT '' COMMENT '原始文件名',
+  `group_` varchar(255) DEFAULT '' COMMENT 'FastDFS返回的组\n用于FastDFS',
+  `path` varchar(255) DEFAULT '' COMMENT 'FastDFS的远程文件名\n用于FastDFS',
+  `relative_path` varchar(255) DEFAULT '' COMMENT '文件相对路径',
+  `url` varchar(255) DEFAULT '' COMMENT '文件访问链接\n需要通过nginx配置路由，才能访问',
+  `file_md5` varchar(255) DEFAULT NULL COMMENT '文件md5值',
+  `context_type` varchar(255) DEFAULT '' COMMENT '文件上传类型\n取上传文件的值',
+  `filename` varchar(255) DEFAULT '' COMMENT '唯一文件名',
+  `ext` varchar(64) DEFAULT '' COMMENT '后缀\n (没有.)',
+  `size` bigint(20) DEFAULT '0' COMMENT '大小',
+  `org_id` bigint(20) DEFAULT NULL COMMENT '组织ID\n#c_core_org',
+  `icon` varchar(64) DEFAULT '' COMMENT '图标',
+  `create_month` varchar(10) DEFAULT NULL COMMENT '创建年月\n格式：yyyy-MM 用于统计',
+  `create_week` varchar(10) DEFAULT NULL COMMENT '创建时处于当年的第几周\nyyyy-ww 用于统计',
+  `create_day` varchar(12) DEFAULT NULL COMMENT '创建年月日\n格式： yyyy-MM-dd 用于统计',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_user` bigint(11) DEFAULT NULL COMMENT '创建人',
+  `update_time` datetime DEFAULT NULL COMMENT '最后修改时间',
+  `update_user` bigint(11) DEFAULT NULL COMMENT '最后修改人',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='附件';
+
+
+DROP TABLE IF EXISTS `pd_file`;
+CREATE TABLE `pd_file` (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `data_type` varchar(255) DEFAULT 'IMAGE' COMMENT '数据类型\n#DataType{DIR:目录;IMAGE:图片;VIDEO:视频;AUDIO:音频;DOC:文档;OTHER:其他}',
+  `submitted_file_name` varchar(255) DEFAULT '' COMMENT '原始文件名',
+  `tree_path` varchar(255) DEFAULT ',' COMMENT '父目录层级关系',
+  `grade` int(11) DEFAULT '1' COMMENT '层级等级\n从1开始计算',
+  `is_delete` bit(1) DEFAULT b'0' COMMENT '是否删除\n#BooleanStatus{TRUE:1,已删除;FALSE:0,未删除}',
+  `folder_id` bigint(20) DEFAULT '0' COMMENT '父文件夹ID',
+  `url` varchar(1000) DEFAULT '' COMMENT '文件访问链接\n需要通过nginx配置路由，才能访问',
+  `size` bigint(20) DEFAULT '0' COMMENT '文件大小\n单位字节',
+  `folder_name` varchar(255) DEFAULT '' COMMENT '父文件夹名称',
+  `group_` varchar(255) DEFAULT '' COMMENT 'FastDFS组\n用于FastDFS',
+  `path` varchar(255) DEFAULT '' COMMENT 'FastDFS远程文件名\n用于FastDFS',
+  `relative_path` varchar(255) DEFAULT '' COMMENT '文件的相对路径 ',
+  `file_md5` varchar(255) DEFAULT '' COMMENT 'md5值',
+  `context_type` varchar(255) DEFAULT '' COMMENT '文件类型\n取上传文件的值',
+  `filename` varchar(255) DEFAULT '' COMMENT '唯一文件名',
+  `ext` varchar(64) DEFAULT '' COMMENT '文件名后缀 \n(没有.)',
+  `icon` varchar(64) DEFAULT '' COMMENT '文件图标\n用于云盘显示',
+  `create_month` varchar(10) DEFAULT NULL COMMENT '创建时年月\n格式：yyyy-MM 用于统计',
+  `create_week` varchar(10) DEFAULT NULL COMMENT '创建时年周\nyyyy-ww 用于统计',
+  `create_day` varchar(12) DEFAULT NULL COMMENT '创建时年月日\n格式： yyyy-MM-dd 用于统计',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_user` bigint(20) DEFAULT NULL COMMENT '创建人',
+  `update_time` datetime DEFAULT NULL COMMENT '最后修改时间',
+  `update_user` bigint(20) DEFAULT NULL COMMENT '最后修改人',
+  `source` varchar(10) DEFAULT 'inner' COMMENT '文件来源：inner, outer',
+  PRIMARY KEY (`id`) USING BTREE,
+  FULLTEXT KEY `FU_TREE_PATH` (`tree_path`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='文件表'
